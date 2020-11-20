4d66a1a4f495d83a25f18c8f61d2f370636c1c61
Kazuya Izumi <shikibu9419@gmail.com>
2020-11-20 18:01:20 +0900

    Set random activity when starting alerm

--- main.ts
+++ main.ts
@@ -1,36 +1,76 @@
+const TIMES_LIMIT = 10
+let continuesAlerm = false
+let activitySuccessCount = 0
+const activityFunctions = [shakeActivityX, shakeActivityY, shakeActivityZ]
+
+// utils
+function getRandomInt(max: number): number {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// events
+input.onButtonPressed(Button.A, function () {
+  startAlermActivity()
+})
+
+// alerms
 function startAlermActivity () {
-    runAlerm()
-    continuesAlerm = true
-    while (continuesAlerm) {
-        continuesAlerm = startShakeActivity()
-    }
-    stopAlerm()
+  continuesAlerm = true
+  // 音を鳴らしたりする
+  runAlerm()
+  // アクティビティをランダムに選択
+  const activity = activityFunctions[getRandomInt(activityFunctions.length)]
+  while (continuesAlerm) {
+    continuesAlerm = activity()
+  }
+  // アラームを止める
+  stopAlerm()
 }
 function runAlerm () {
-    basic.showIcon(IconNames.Angry)
+  basic.showIcon(IconNames.Angry)
 }
 function stopAlerm () {
-    basic.showIcon(IconNames.Happy)
+  basic.showIcon(IconNames.Happy)
 }
-input.onButtonPressed(Button.A, function () {
-    startAlermActivity()
-})
-function startShakeActivity () {
-    preZ = input.acceleration(Dimension.Z)
-    basic.pause(1000)
-    if (Math.abs(preZ - input.acceleration(Dimension.Z)) > 200) {
-        serial.writeString("OK.")
-        serial.writeValue("times", activitySuccessTimes)
-        activitySuccessTimes += 1
-    } else {
-        activitySuccessTimes = 0
-    }
-    return activitySuccessTimes < TIMES_LIMIT
+
+// activities
+function shakeActivityX () {
+  shakeActivity('x')
 }
-let preZ = 0
-let activitySuccessTimes = 0
-let continuesAlerm = false
-let TIMES_LIMIT = 0
-TIMES_LIMIT = 10
-continuesAlerm = false
-activitySuccessTimes = 0
+
+function shakeActivityY () {
+  shakeActivity('y')
+}
+
+function shakeActivityZ () {
+  shakeActivity('z')
+}
+
+function shakeActivity (direction: 'x' | 'y' | 'z') {
+  serial.writeString(`Shake in the ${direction}-axis direction.`)
+
+  const dimension = getDimension(direction)
+  const preAccel = input.acceleration(dimension)
+  basic.pause(1000)
+
+  // 加速度の差が一定量を超えたらインクリメント
+  if (Math.abs(preAccel - input.acceleration(dimension)) > 200) {
+    serial.writeString("OK.")
+    serial.writeValue("times", activitySuccessCount)
+    activitySuccessCount += 1
+  } else {
+    activitySuccessCount = 0
+  }
+  return activitySuccessCount < TIMES_LIMIT
+}
+
+function getDimension (direction: 'x' | 'y' | 'z') {
+  switch (direction) {
+    case 'x':
+      return Dimension.X
+    case 'y':
+      return Dimension.Y
+    case 'z':
+      return Dimension.Z
+  }
+}
