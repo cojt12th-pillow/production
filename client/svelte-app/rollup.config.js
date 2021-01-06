8b1de6cc396f2a6fe3c316d096fd2f38d4e8525f
Kazuya Izumi <shikibu9419@gmail.com>
2021-01-06 13:46:56 +0900

    Setup node-sass

--- client/svelte-app/rollup.config.js
+++ client/svelte-app/rollup.config.js
@@ -4,6 +4,16 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import sveltePreprocess from 'svelte-preprocess';
+
+const preprocess = sveltePreprocess({
+  scss: {
+    includePaths: ['src'],
+  },
+  postcss: {
+    plugins: [require('autoprefixer')],
+  },
+});
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -41,7 +51,8 @@ export default {
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
-			}
+			},
+			preprocess,
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
