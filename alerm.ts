c10928cfc558b3e44b5d9f9a6b2d78095ecfa52d
Kazuya Izumi <shikibu9419@gmail.com>
2021-02-04 02:45:58 +0900

    Fix alerm

--- alerm.ts
+++ alerm.ts
@@ -1,4 +1,5 @@
 let todayAlermFinished = false
+
 // e.g. 12:00 on Mon / Tue / Wed
 let hour = EEPROM.getAnyEEP(EEPROM.DS1307_REG_RAM1);
 let minute = EEPROM.getAnyEEP(EEPROM.DS1307_REG_RAM2);
@@ -6,21 +7,36 @@ let weekdays = EEPROM.getDayEEP();
 
 serial.writeLine(`INIT: ${hour}:${minute} in (${weekdays.join(',')})`)
 
-const activityFunctions: (() => void)[] = [shakeActivityX, shakeActivityY, shakeActivityZ]
+// DS1307.stop()
+// basic.pause(600)
+// DS1307.stop()
+// basic.pause(600)
+// DS1307.stop()
+// basic.pause(600)
+// DS1307.start()
+
+// const activityFunctions: (() => void)[] = [shakeActivityX, shakeActivityY, shakeActivityZ]
 
 // alerms
 function startAlermActivity () {
+  pins.digitalWritePin(DigitalPin.P11, 1)
+  pins.digitalWritePin(DigitalPin.P12, 0)
+
+  serial.writeLine('START ALERM!!')
   // アクティビティをランダムに選択
-  setLoopMode(1)
   playVoice(trackCategory.start)
 
   // 再生時間だけ待機
+  basic.pause(10 * 1000)
 
-  const number = getRandomInt(3)
-  activityFunctions[number]();
+  // const number = getRandomInt(4)
+  shakeActivityY();
 
   // アラームを止める
   stopAlerm()
+
+  pins.digitalWritePin(DigitalPin.P11, 0)
+  pins.digitalWritePin(DigitalPin.P12, 1)
 
   playVoice(trackCategory.finish)
 }
@@ -53,6 +69,7 @@ function getAlermInfo() {
 
   // 曜日と時刻が該当したらアラームを起動
   if (hour === currentHour && minute === currentMin) {
+    serial.writeLine('WAKE UP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     todayAlermFinished = true
     // startAlermActivity()
   }
@@ -66,8 +83,11 @@ function setAlermTime(value: string) {
   weekdays = values[2].split('');
 
   EEPROM.setAnyEEP(EEPROM.DS1307_REG_RAM1, hour);
+  basic.pause(5);
   EEPROM.setAnyEEP(EEPROM.DS1307_REG_RAM2, minute);
+  basic.pause(5);
   EEPROM.setDayEEP(weekdays);
+  basic.pause(5);
 
   serial.writeLine(`data setted: [${values.join(', ')}]`)
 }
@@ -75,5 +95,18 @@ function setAlermTime(value: string) {
 function setRTCDate(value: string) {
   const values = value.split(',').map(v => parseInt(v));
 
-  DS1307.DateTime(2021, values[0], values[1], values[2], values[3], values[4], values[5]);
+  DS1307.setYear(2021)
+  basic.pause(5);
+  DS1307.setMonth(values[0])
+  basic.pause(5);
+  DS1307.setDay(values[1])
+  basic.pause(5);
+  DS1307.setWeekday(values[2])
+  basic.pause(5);
+  DS1307.setHour(values[3])
+  basic.pause(5);
+  DS1307.setMinute(values[4])
+  basic.pause(5);
+  DS1307.setSecond(values[5])
+  basic.pause(5);
 }
